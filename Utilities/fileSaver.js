70c40e89b8471dc27a08ea989169f40259ba1a0b
jayanth-14 <jayanthpetchetti@gmail.com>
2025-06-25 00:13:11 +0530

    implement genric function to do saving and uploading of files

---
+++ Utilities/fileSaver.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require("fs");
+const uploadObject = require('../minio/uploadFiles');
+
+const fileSaver = async (file, name, folder) => {
+  try {
+    const tempPath = path.join(process.cwd(), "temp");
+    // checking if the temporary folder exists and if not creatin it.
+    if (!fs.existsSync(tempPath)) {
+      fs.mkdirSync(tempPath, { recursive: true });
+    }
+    // creating a new path which includes the file and saving it into filesystem
+    const tempFilePath = path.join(tempPath, name+path.extname(file.name));
+    await file.mv(tempFilePath);
+    const bucketName = process.env.MINIO_BUCKET;
+    // uploading the file into the minio bucket
+    await uploadObject(tempFilePath, "ebooks", bucketName);
+    // deleting the temporary folder.
+    fs.unlinkSync(tempFilePath);
+    return `${bucketName}/${folder}/${name}`;
+  } catch (error) {
+    console.log("error in saving the file");
+    throw error;
+  }
+};
+
+module.exports = fileSaver;
--- controllers/ebook.js
+++ controllers/ebook.js
@@ -4,12 +4,13 @@ const queries = require("../database/queries");
 const path = require('path');
 const fs = require("fs");
 const uploadObject = require("../minio/uploadFiles");
+const fileSaver = require("../utilities/fileSaver");
 
 const addEbook = async (req, res) => {
   const { title, author, link } = req.body;
   try {
     if (link && !req.files?.ebook) {
-      await db.execute(queries.addEbook, [
+     await db.execute(queries.addEbook, [
         title, author, link
       ]);
       return res.status(201).json({ message: "Ebook added successfully" });
@@ -18,32 +19,13 @@ const addEbook = async (req, res) => {
     if (!req.files?.ebook) {
       return res.status(400).json({ error: "No ebook file provided" });
     }
-
-    const tempPath = path.join(process.cwd(), "temp");
-    if (!fs.existsSync(tempPath)) {
-      fs.mkdirSync(tempPath, { recursive: true });
-    }
-
-    const tempFilePath = path.join(tempPath, req.files.ebook.name);
-    await req.files.ebook.mv(tempFilePath);
-
     try {
-      const bucketName = "gptcvrm";
-      await uploadObject(tempFilePath, "ebooks", bucketName);
-      
-      const ebookPath = `${bucketName}/ebooks/${req.files.ebook.name}`;
-      
+      const ebookPath = await fileSaver(req.files.ebook,title, "ebooks")
       await db.execute(queries.addEbook, [
         title, author, ebookPath
       ]);
-      
-      fs.unlinkSync(tempFilePath);
-      
       return res.status(201).json({ message: "Ebook added successfully" });
     } catch (uploadError) {
-      if (fs.existsSync(tempFilePath)) {
-        fs.unlinkSync(tempFilePath);
-      }
       throw uploadError;
     }
   } catch (error) {
