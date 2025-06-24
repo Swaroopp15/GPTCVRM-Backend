13deb66e8570b8652c305406a272c9da21904109
jayanth-14 <jayanthpetchetti@gmail.com>
2025-06-25 00:34:29 +0530

    implement creating of presigned urls

---
+++ Utilities/getPresignedUrl.js
@@ -0,0 +1,11 @@
+const minioClient = require("../minio/connectMinio");
+
+const getPresignedUrl = async (fileName, method = "GET",expiry = 60*60) => {
+  try {
+    const url = await minioClient.presignedUrl(method, process.env.MINIO_BUCKET, fileName, expiry);
+    return url;
+  } catch (error) {
+    console.log("Error in creating a presigned url : ", error);
+    throw error;
+  }
+}
